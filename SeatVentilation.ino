884e4cb75bd22a06f8b792542e0a40243f41315f
marsel.khabibullin.99 <marsel.khabibullin.99@gmail.com>
2024-12-02 02:30:41 +0300

    Big update

--- SeatVentilation.ino
+++ SeatVentilation.ino
@@ -1,22 +1,34 @@
 #include <EEPROM.h>
 
-int menu=0;//0-def; 1-speedLow; 2-speedMid; 3-speedHigh; 4-wellcome; 5-ventilationMemory; 6-windshieldMemory
+int menu=0;//0-def; 1-speedLow; 2-speedMid; 3-speedHigh; 4-ventilationMemory; 5-wheelMemory; 6-windshieldMemory; 7-wswTypeOfSwitch; 8-autoVent; 9-autoWheel; 10-autoWindShield; 11-wellcome; 12-diagnostic
 bool isMenuEdit=false;
 int editValue=0;
 int menuQuarz=0;
-
-bool emulateIndicators=true;
+int menu3Quarz=0;
+const int menuQuartzMax=20;//1sec
+const int menu3QuartzMax=100;//5sec
+const int maxMenuItem=12;
 
 struct Memory{
-  int lowSpeed=10;
-  int midSpeed=8;
-  int highSpeed=6;
-  int wellcomeState=1;
-  int ventilationStateMemory=1;
-  int ventilationState1=1;
-  int ventilationState2=2;
-  int wsStateMemory=0;
-  int wsState=0;
+  int lowSpeed = 10;
+  int midSpeed = 8;
+  int highSpeed = 6;
+
+  int ventilationStateMemory = 1;
+  int ventilationState1 = 1;
+  int ventilationState2 = 2;
+
+  int wsStateMemory = 0;
+  int wsState = 0;
+
+  int wStateMemory = 0;
+  int wState = 0;
+
+  int wswTypeOfSwitch=0;
+  int autoVentilation = 0;
+  int autoWheel = 0;
+  int autoWindShield = 0;
+  int wellcomeState = 1;
 } memory;
 
 struct Seat{
@@ -41,12 +53,12 @@ Seat seat2;
 struct Wheel{
   int btn=A2;
   int btnPressCounter=0;
-  int led=A3;
+  int led = 3;
   int ledMode=0;//0-idle; 1-w; 2-ws; 3-s
-  int wSignal;
-  int wsSignal;
-  int wIndicator;
-  int wsIndicator;
+  int wSignal = 4;
+  int wsSignal = 2;
+  int wIndicator = A3;
+  int wsIndicator = A4;
 } wheel;
 
 void setup()
@@ -56,9 +68,9 @@ void setup()
 
   seat1.btnPin=A0;
   seat1.fanPwmPin=5;
-  seat1.lowLed=6;
+  seat1.lowLed=8;
   seat1.midLed=7;
-  seat1.highLed=8;
+  seat1.highLed=6;
 
   seat2.btnPin=A1;
   seat2.fanPwmPin=9;
@@ -80,13 +92,50 @@ void setup()
 
   pinMode(wheel.btn, INPUT);
   pinMode(wheel.led, OUTPUT);
+  pinMode(wheel.wIndicator, INPUT);
+  pinMode(wheel.wsIndicator, INPUT);
+  pinMode(wheel.wSignal, OUTPUT);
+  pinMode(wheel.wSignal, OUTPUT);
 
   EEPROM.get(0, memory);
   if(memory.lowSpeed==255)
   {
     Memory newMemory;
     EEPROM.put(0, newMemory);
-    Serial.println("new memory");
+    log("New memory", 0);
+  }
+  else
+  {
+    if(clamp(memory.lowSpeed, 0, 12) != memory.lowSpeed)
+      err("Out of range lowSpeed", memory.lowSpeed);
+    if(clamp(memory.midSpeed, 0, 12) != memory.midSpeed)
+      err("Out of range midSpeed", memory.midSpeed);
+    if(clamp(memory.highSpeed, 0, 12) != memory.highSpeed)
+      err("Out of range highSpeed", memory.highSpeed);
+    if(clamp(memory.ventilationStateMemory, 0, 1) != memory.ventilationStateMemory)
+      err("Out of range ventilationStateMemory", memory.ventilationStateMemory);
+    if(clamp(memory.ventilationState1, 0, 3) != memory.ventilationState1)
+      err("Out of range ventilationState1", memory.ventilationState1);
+    if(clamp(memory.ventilationState2, 0, 3) != memory.ventilationState2)
+      err("Out of range ventilationState2", memory.ventilationState2);
+    if(clamp(memory.wsStateMemory, 0, 1) != memory.wsStateMemory)
+      err("Out of range wsStateMemory", memory.wsStateMemory);
+    if(clamp(memory.wsState, 0, 1) != memory.wsState)
+      err("Out of range wsState", memory.wsState);
+    if(clamp(memory.wStateMemory, 0, 1) != memory.wStateMemory)
+      err("Out of range wStateMemory", memory.wStateMemory);
+    if(clamp(memory.wState, 0, 1) != memory.wState)
+      err("Out of range wState", memory.wState);
+    if(clamp(memory.wswTypeOfSwitch, 0, 1) != memory.wswTypeOfSwitch)
+      err("Out of range wswTypeOfSwitch", memory.wswTypeOfSwitch);
+    if(clamp(memory.autoVentilation, 0, 12) != memory.autoVentilation)
+      err("Out of range autoVentilation", memory.autoVentilation);
+    if(clamp(memory.autoWheel, 0, 12) != memory.autoWheel)
+      err("Out of range autoWheel", memory.autoWheel);
+    if(clamp(memory.autoWindShield, 0, 12) != memory.autoWindShield)
+      err("Out of range autoWindShield", memory.autoWindShield);
+    if(clamp(memory.wellcomeState, 0, 1) != memory.wellcomeState)
+      err("Out of range wellcomeState", memory.wellcomeState);
   }
 
   Serial.println(memory.lowSpeed);
@@ -106,7 +155,12 @@ void setup()
 
   if(memory.wsStateMemory)
   {
-    wheel.ledMode=memory.wsState;
+    wsClickBtn();
+  }
+
+  if(memory.wStateMemory)
+  {
+    wClickBtn();
   }
 }
 
@@ -115,9 +169,9 @@ void loop()
   int event1=getBtnEvent(&seat1);
   int event2=getBtnEvent(&seat2);
   int eventWheel=getBtnEvent(&wheel);
-  if(event1!=0) Serial.println(event1);
-  if(event2!=0) Serial.println(event2);
-  if(eventWheel!=0) Serial.println(eventWheel);
+  if(event1!=0) log("Vent1", event1);
+  if(event2!=0) log("Vent2", event2);
+  if(eventWheel!=0) log("Wheel", eventWheel);
   if(menu==0)
   {
     if(event1==-1)
@@ -138,19 +192,19 @@ void loop()
     if(event1==-1)
     {
       menu--;
-      if(menu<1) menu=6;
+      if(menu<1) menu=maxMenuItem;
     }
     if(event2==-1)
     {
       menu++;
-      if(menu>6) menu=1;
+      if(menu>maxMenuItem) menu=1;
     }
     if(event1==2)
     {
       isMenuEdit=true;
       editValue=getValue(menu);
     }
-    setBar(menu*2-1);
+    setBar(menu);
 
     //exit to def
     if(event2==2)
@@ -165,11 +219,11 @@ void loop()
   {
     if(event1==-1)
     {
-      editValue=clamp(--editValue, 0, menu<4?12:1);
+      editValue=clamp(--editValue, 0, (menu<=3 | menu>=8&&menu<=10) ? 12 : 1);
     }
     if(event2==-1)
     {
-      editValue=clamp(++editValue, 0, menu<4?12:1);
+      editValue=clamp(++editValue, 0, (menu<=3 | menu>=8&&menu<=10) ? 12 : 1);
     }
     if(menu<4)
     {
@@ -190,62 +244,20 @@ void loop()
     setBar(editValue);
   }
 
-  if(eventWheel==-1)
+  if(memory.wswTypeOfSwitch==0)
+    wswSwitch0(eventWheel);
+  else if(memory.wswTypeOfSwitch==1)
+    wswSwitch1(eventWheel);
+  else
   {
-    int wI=getWI();
-    int wsI=getWSI();
-    if(wI==LOW && wsI==LOW)
-    {
-      //руль
-      wheel.ledMode=1;
-    }
-    else if(wI==HIGH && wsI==LOW)
-    {
-      //выкл
-      wheel.ledMode=0;
-    }
-    else if(wI==LOW && wsI==HIGH)
-    {
-      //руль+стекло
-      wheel.ledMode=2;
-    }
-    else if(wI==HIGH && wsI==HIGH)
-    {
-      //стекло
-      wheel.ledMode=3;
-    }
-    memoryMode(&wheel);
-  }
-  else if(eventWheel==2)
-  {
-    int wI=getWI();
-    int wsI=getWSI();
-    if(wI==LOW && wsI==LOW)
-    {
-      //руль+стекло
-      wheel.ledMode=2;
-    }
-    else if(wI==HIGH && wsI==LOW)
-    {
-      //руль+стекло
-      wheel.ledMode=2;
-    }
-    else if(wI==LOW && wsI==HIGH)
-    {
-      //выкл
-      wheel.ledMode=0;
-    }
-    else if(wI==HIGH && wsI==HIGH)
-    {
-      //руль
-      wheel.ledMode=1;
-    }
-    memoryMode(&wheel);
+    err("Missed typeOfSwitch", memory.wswTypeOfSwitch);
   }
   setWheelIndicator();
 
   menuQuarz++;
-  if(menuQuarz>20) menuQuarz=0;
+  if(menuQuarz>menuQuartzMax) menuQuarz=0;
+  menu3Quarz++;
+  if(menu3Quarz>menu3QuartzMax) menu3Quarz=0;
   delay(50);
 }
 
@@ -253,7 +265,8 @@ void loop()
 int getBtnEvent(Seat* seat)
 {
   int pinValue=analogRead(seat->btnPin);
-  if(pinValue<500)
+
+  if(pinValue<900)
   {
     seat->btnPressCounter++;
     if(seat->btnPressCounter==30){
@@ -285,7 +298,8 @@ int getBtnEvent(Seat* seat)
 int getBtnEvent(Wheel* wheel)
 {
   int pinValue=analogRead(wheel->btn);
-  if(pinValue<500)
+
+  if(pinValue<900)
   {
     wheel->btnPressCounter++;
     if(wheel->btnPressCounter==30){
@@ -315,39 +329,157 @@ int getBtnEvent(Wheel* wheel)
 
 void setWheelIndicator()
 {
+  //0-idle; 1-w; 2-ws; 3-s
+  int wI=getWI();
+  int wsI=getWSI();
+  if(wI == LOW)
+  {
+    wheel.ledMode = wsI==LOW
+      ? 0
+      : 3;
+  }
+  else
+  {
+    wheel.ledMode = wsI==LOW
+      ? 1
+      : 2;
+  }
+
   int pwm=0;
   if(wheel.ledMode==0)
     pwm=0;
   else if(wheel.ledMode==1)
-    pwm=getWheelPwm();
+    pwm=255.0f / (menu3QuartzMax/2) * min(menu3QuartzMax/2, 0+abs(menu3Quarz-(menu3QuartzMax/2)));
   else if(wheel.ledMode==2)
     pwm=255;
   else if(wheel.ledMode==3)
-    pwm=menuQuarz>2?255:0;
+    pwm=255.0f / (menuQuartzMax/2) * min(menuQuartzMax/2, 0+abs(menuQuarz-(menuQuartzMax/2)));
   
-  log("Led", wheel.ledMode, pwm);
-  analogWrite(wheel.led, pwm);
+  setWheelIndicator(pwm);
 }
 
-int getWheelPwm()
+void setWheelIndicator(int pwm)
 {
-  if(menuQuarz>=0 & menuQuarz<=2 || menuQuarz>=7 & menuQuarz<=9)
-    return 255;
-  else return 0;
+  //log("Led", wheel.ledMode, pwm);
+  analogWrite(wheel.led, pwm);
 }
 
 int getWI()
 {
-  return emulateIndicators
-    ? (int)(wheel.ledMode==1 || wheel.ledMode==2)
-    : digitalRead(wheel.wIndicator);
+  return analogRead(wheel.wIndicator) > 500
+    ? HIGH
+    : LOW;
 }
 
 int getWSI()
 {
-  return emulateIndicators
-    ? (int)(wheel.ledMode==2 || wheel.ledMode==3)
-    : digitalRead(wheel.wsIndicator);
+  return analogRead(wheel.wsIndicator) > 500
+    ? HIGH
+    : LOW;
+}
+
+void wswSwitch0(int eventWheel)
+{
+  if(eventWheel==0 || eventWheel==1)
+    return;
+
+  int wI=getWI();
+  int wsI=getWSI();
+  log("wswi0", wI, wsI);
+
+  if(eventWheel==-1)
+  {
+    //руль переключить
+    wClickBtn();
+  }
+  else if(eventWheel==2)
+  {
+    if(wI==LOW && wsI==LOW)
+    {
+      //руль+стекло включить
+      wsClickBtn();
+      wClickBtn();
+    }
+    else
+    {
+      //стекло переключить
+      wsClickBtn();
+    }
+  }
+
+  setWheelIndicator(0);
+  delay(50);
+  setWheelIndicator(255);
+
+  memoryMode(&wheel);
+}
+
+void wswSwitch1(int eventWheel)
+{
+  if(eventWheel==0 || eventWheel==1)
+    return;
+
+  int wI=getWI();
+  int wsI=getWSI();
+  log("wswi1", wI, wsI);
+
+  if(eventWheel==-1)
+  {
+    if(wI==LOW && wsI==LOW)
+    {
+      //руль включить
+      wClickBtn();
+    }
+    else if(wI==HIGH && wsI==LOW)
+    {
+      //стекло включить
+      wsClickBtn();
+    }
+    else if(wI==HIGH && wsI==HIGH)
+    {
+      //руль выключить
+      wClickBtn();
+    }
+    else if(wI==LOW && wsI==HIGH)
+    {
+      //стекло выключить
+      wsClickBtn();
+    }
+  }
+  else if(eventWheel==2)
+  {
+    if(wI==HIGH)
+    {
+      //руль выключить
+      wClickBtn();
+    }
+    if(wsI==HIGH)
+    {
+      //стекло выключить
+      wsClickBtn();
+    }
+  }
+
+  setWheelIndicator(0);
+  delay(100);
+  setWheelIndicator(255);
+  delay(100);
+
+  memoryMode(&wheel);
+}
+
+void wClickBtn()
+{
+  digitalWrite(wheel.wSignal, HIGH);
+  delay(50);
+  digitalWrite(wheel.wSignal, LOW);
+}
+
+void wsClickBtn()
+{
+  digitalWrite(wheel.wsSignal, HIGH);
+  delay(50);
+  digitalWrite(wheel.wsSignal, LOW);
 }
 
 void nextMode(Seat* seat)
@@ -425,6 +557,7 @@ void setBar(int bar)
   digitalWrite(seat2.lowLed, ((bar==11 & menuQuarz<10==0)|(bar>11))?HIGH:LOW);
 }
 
+//Приветствие
 void wellcome()
 {
   digitalWrite(seat1.lowLed, LOW);
@@ -459,6 +592,7 @@ int clamp(int val, int min, int max)
   return val<min?min:(val>max?max:val);
 }
 
+//Получить настройку из памяти
 int getValue(int menu)
 {
   if(menu==1)
@@ -468,15 +602,29 @@ int getValue(int menu)
   if(menu==3)
     return memory.highSpeed;
   if(menu==4)
-    return memory.wellcomeState;
-  if(menu==5)
     return memory.ventilationStateMemory;
+  if(menu==5)
+    return memory.wStateMemory;
   if(menu==6)
     return memory.wsStateMemory;
+  if(menu==7)
+    return memory.wswTypeOfSwitch;
+  if(menu==8)
+    return memory.autoVentilation;
+  if(menu==9)
+    return memory.autoWheel;
+  if(menu==10)
+    return memory.autoWindShield;
+  if(menu==11)
+    return memory.wellcomeState;
+  if(menu==12)
+    return 1;
 
+  err("GetValue imposible", menu);
   return 0; //imposible
 }
 
+//Сохранить настройку в памяти
 void saveValue(int menu)
 {
   if(menu==1)
@@ -486,11 +634,26 @@ void saveValue(int menu)
   if(menu==3)
     memory.highSpeed=editValue;
   if(menu==4)
-    memory.wellcomeState=editValue;
-  if(menu==5)
     memory.ventilationStateMemory=editValue;
+  if(menu==5)
+    memory.wStateMemory=editValue;
   if(menu==6)
     memory.wsStateMemory=editValue;
+  if(menu==7)
+    memory.wswTypeOfSwitch=editValue;
+  if(menu==8)
+    memory.autoVentilation=editValue;
+  if(menu==9)
+    memory.autoWheel=editValue;
+  if(menu==10)
+    memory.autoWindShield=editValue;
+  if(menu==11)
+    memory.wellcomeState=editValue;
+  if(menu==12)
+  {
+    selfTest();
+    return;
+  }
 
   EEPROM.put(0, memory);
 
@@ -498,22 +661,42 @@ void saveValue(int menu)
   setVentilation(seat2);
 }
 
+//Сохранить состояние вентиляций
 void memoryMode(Seat* seat)
 {
+  if(memory.ventilationStateMemory!=1)
+    return;
   if(seat==&seat1)
-    memory.ventilationState1=seat->mode;
+  {
+    if(memory.ventilationState1 != seat->mode)
+    {
+      memory.ventilationState1=seat->mode;
+      EEPROM.put(0, memory);
+    }
+  }
   else if(seat==&seat2)
-    memory.ventilationState2=seat->mode;
+  {
+    if(memory.ventilationState2 != seat->mode)
+    {
+      memory.ventilationState2=seat->mode;
+      EEPROM.put(0, memory);
+    }
+  }
+}
 
-  if(memory.ventilationStateMemory==1)
+//Сохранить состояние руля и лобаша
+void memoryMode(Wheel* wheel)
+{
+  bool hadChanged = memory.wsState != getWSI() || memory.wState != getWI();
+  memory.wsState = getWSI();
+  memory.wState = getWI();
+  if(memory.wsStateMemory==1 && hadChanged)
     EEPROM.put(0, memory);
 }
 
-void memoryMode(Wheel* wheel)
+void selfTest()
 {
-  memory.wsState=wheel->ledMode;
-  if(memory.wsStateMemory==1)
-    EEPROM.put(0, memory);
+
 }
 
 void log(String msg, int val)
@@ -528,4 +711,10 @@ void log(String msg, int val, int val2)
   Serial.print(val);
   Serial.print(", ");
   Serial.println(val2);
+}
+
+void err(String msg, int val)
+{
+  Serial.print("ERR: " + msg + ": ");
+  Serial.println(val);
 }
